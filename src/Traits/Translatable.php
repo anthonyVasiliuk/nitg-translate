ece4920c9a470da505c784b361d809c9a241b54e
Antony Vasiliuk <anthony.vbrest@gmail.com>
2024-01-05 12:08:26 +0300

    add booted method to trait

--- src/Traits/Translatable.php
+++ src/Traits/Translatable.php
@@ -3,6 +3,7 @@
 namespace Nitg\NitgTranslate\Traits;
 
 
+use Illuminate\Database\Eloquent\Builder;
 use Nitg\NitgTranslate\Models\Translate;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
@@ -11,5 +12,15 @@ trait Translatable
     public function translate(): MorphMany
     {
         return $this->morphMany(Translate::class, 'translatable');
+    }
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope('locale', function (Builder $builder) {
+            $lang = request()->header('localization');
+            if ($lang && $lang !== 'en') {
+                $builder->with('translate');
+            }
+        });
     }
 }
